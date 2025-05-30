1fdb262445f870050b21be96b64f1c60e006819c
maximum0 <maximum.zero95@gmail.com>
2025-05-30 16:23:03 +0900

    feat: Collector 모듈 추가

---
+++ collector/src/main/java/kernel360/ckt/collector/CollectorApplication.java
@@ -0,0 +1,16 @@
+package kernel360.ckt.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@EntityScan(basePackages = "kernel360.ckt.core")
+@ComponentScan(basePackages = {"kernel360.ckt.core"})
+@SpringBootApplication
+public class CollectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectorApplication.class, args);
+    }
+}
