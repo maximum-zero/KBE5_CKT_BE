7a76e9684666820ac3db5790ff5fc19196562dee
LeeHyunKyeong <s2s20p@naver.com>
2025-06-04 14:24:11 +0900

    chore: gateway config에 드라이빙 로그 추가

--- gateway/src/main/java/kernel360/ckt/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/kernel360/ckt/gateway/config/GatewayConfig.java
@@ -29,6 +29,9 @@ public class GatewayConfig {
             .route("vehicle", r -> r.path("/api/v1/vehicles/**")
                 .uri("http://admin:8080"))
 
+            .route("driving-log", r -> r.path("/api/v1/logs/drive/**")
+                .uri("http://admin:8080"))
+
             .build();
     }
 }
