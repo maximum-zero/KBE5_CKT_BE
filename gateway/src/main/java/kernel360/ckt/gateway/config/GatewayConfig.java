817fcbab4665465c96de53f338ca7c5ffd56eb48
maximum-zero <maximum.zero95@gmail.com>
2025-06-10 17:10:22 +0900

    chore: 환경 설정 분리 및 변경 (#43)
    * chore: gateway 모듈 환경 설정
    
    - customRoutes 기능 제거 및 application 파일에서 제어 하도록 변경
    - local, docker, prod 환경 분리
    
    * chore: auth 모듈 환경 설정
    
    - local, prod 환경 분리
    
    * chore: admin 모듈 환경 설정
    
    - local, prod 환경 분리
    - port 변경
    
    * chore: collector 모듈 환경 설정
    
    - local, prod 환경 분리
    
    * chore: docker compose 설정 변경
    
    - profiles active 환경변수 추가
    - 모듈별 포트 변경

--- gateway/src/main/java/kernel360/ckt/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/kernel360/ckt/gateway/config/GatewayConfig.java
@@ -1,71 +1,26 @@
 package kernel360.ckt.gateway.config;
 
-import kernel360.ckt.gateway.filter.JwtAuthenticationFilter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
 @Configuration
 @RequiredArgsConstructor
 public class GatewayConfig {
 
-    private final JwtAuthenticationFilter jwtGatewayFilter;
-
-    @Value("${backend.auth-url}")
-    private String authUrl;
-
-    @Value("${backend.admin-url}")
-    private String adminUrl;
-
-    @Value("${backend.collector-url}")
-    private String collectorUrl;
-
-    @Value("${backend.frontend-origin}")
-    private String frontendOrigin;
-
-    @Bean
-    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-
-            .route("auth", r -> r.path("/api/v1/auth/**")
-                .uri(authUrl))
-
-            .route("collector", r -> r.path("/api/v1/vehicles/collector/**")
-                .uri(collectorUrl))
-
-            .route("admin", r -> r.path("/api/v1/companies/**")
-                .uri(adminUrl))
-
-            .route("customer", r -> r.path("/api/v1/customers/**")
-                .uri(adminUrl))
-
-            .route("vehicle", r -> r.path("/api/v1/vehicles/**")
-                .uri(adminUrl))
-
-            .route("rental", r -> r.path("/api/v1/rentals/**")
-                .uri(adminUrl))
-
-            .route("logs", r -> r.path("/api/v1/logs/**")
-                .uri("http://admin:8080"))
-
-            .route("driving-log", r -> r.path("/api/v1/logs/drive/**")
-                .uri("http://admin:8080"))
-
-            .build();
-    }
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+  
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(List.of(frontendOrigin));
+        config.setAllowedOriginPatterns(List.of(allowedOrigins));
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
@@ -75,4 +30,3 @@ public class GatewayConfig {
         return new CorsWebFilter(source);
     }
 }
-
