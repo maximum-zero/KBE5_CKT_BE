5aa65efdcf19b1ffaf067b8a601a4fb5bfb1ddef
maximum0 <maximum.zero95@gmail.com>
2025-06-16 16:16:50 +0900

    refactor: Vehicle Trace Log Repository admin 모듈내 제거

--- admin/src/main/java/kernel360/ckt/admin/infra/VehicleTraceLogRepository.java
+++
@@ -1,7 +0,0 @@
-package kernel360.ckt.admin.infra;
-
-import kernel360.ckt.core.domain.entity.VehicleTraceLogEntity;
-
-public interface VehicleTraceLogRepository {
-    VehicleTraceLogEntity save(VehicleTraceLogEntity vehicleTraceLog);
-}
