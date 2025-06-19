9fcc952a326a59e92d99c02ec795cd2b9fc7a736
maximum0 <maximum.zero95@gmail.com>
2025-06-19 17:35:28 +0900

    feat: 유연한 오류 메시지 처리를 위한 CustomException 개선

--- admin/src/main/java/kernel360/ckt/admin/common/exception/GlobalExceptionHandler.java
+++ admin/src/main/java/kernel360/ckt/admin/common/exception/GlobalExceptionHandler.java
@@ -13,8 +13,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
-        ErrorCode errorCode = ex.getErrorCode();
-        return ResponseEntity.ok(ErrorResponse.from(errorCode));
+        ErrorCode error = ex.getErrorCode();
+        return ResponseEntity.ok(ErrorResponse.from(error.getCode(), ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
