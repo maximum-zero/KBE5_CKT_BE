c00d419986c9f22e086f695f4c952c9f962d1a2f
maximum0 <maximum.zero95@gmail.com>
2025-06-23 06:19:15 +0900

    refactor: VehicleCollectorController 주석 및 로깅 추가

--- collector/src/main/java/kernel360/ckt/collector/ui/VehicleCollectorController.java
+++ collector/src/main/java/kernel360/ckt/collector/ui/VehicleCollectorController.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 차량 주행 API Controller
+ *
+ * 이 클래스는 클라이언트의 HTTP 요청을 받아 예약 관련 비즈니스 로직을 처리하는 진입점 역할을 합니다.
+ * 차량의 시동 ON/OFF, 주기적인 위치 정보 전송 등의 요청을 처리합니다.
+ *
+ */
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/vehicles/collector")
@@ -24,20 +31,41 @@ public class VehicleCollectorController {
 
     private final VehicleCollectorService vehicleCollectorService;
 
+    /**
+     * 차량 시동 ON 정보 전송 API
+     *
+     * @param request 차량 시동 ON 요청 정보 {@link VehicleCollectorOnRequest} DTO
+     * @return 차량 시동 ON 처리 결과 {@link VehicleCollectorResponse}
+     */
     @PostMapping("/on")
     CommonResponse<VehicleCollectorResponse> sendVehicleOn(@RequestBody VehicleCollectorOnRequest request) {
+        log.info("차량 시동 ON 요청 - {}", request);
         final VehicleCollectorOnCommand command = request.toCommand();
         return CommonResponse.success(vehicleCollectorService.sendVehicleOn(command));
     }
 
+    /**
+     * 차량 시동 OFF 정보 전송 API
+     *
+     * @param request 차량 시동 OFF 요청 정보 {@link VehicleCollectorOffRequest} DTO
+     * @return 차량 시동 OFF 처리 결과 {@link VehicleCollectorResponse}
+     */
     @PostMapping("/off")
     CommonResponse<VehicleCollectorResponse> sendVehicleOff(@RequestBody VehicleCollectorOffRequest request) {
+        log.info("차량 시동 OFF 요청 - {}", request);
         final VehicleCollectorOffCommand command = request.toCommand();
         return CommonResponse.success(vehicleCollectorService.sendVehicleOff(command));
     }
 
+    /**
+     * 차량 주기적 위치 정보 저장 API
+     *
+     * @param request 차량 주기적 위치 정보 요청 정보 {@link VehicleCollectorCycleRequest} DTO
+     * @return 차량 주기적 위치 정보 저장 결과 {@link VehicleCollectorResponse}
+     */
     @PostMapping("/cycle")
     CommonResponse<VehicleCollectorResponse> saveVehicleCycle(@RequestBody VehicleCollectorCycleRequest request) {
+        log.info("차량 주기적 위치 정보 저장 요청 - {}", request);
         final VehicleCollectorCycleCommand cycleCommand = request.toCommand();
         return CommonResponse.success(vehicleCollectorService.saveVehicleCycle(cycleCommand));
     }
