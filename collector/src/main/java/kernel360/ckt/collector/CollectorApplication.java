232c969efc8af3da12f3c51d00000b61e6f46eca
maximum0 <maximum.zero95@gmail.com>
2025-06-23 16:06:41 +0900

    refactor: JPA 설정 분리

--- collector/src/main/java/kernel360/ckt/collector/CollectorApplication.java
+++ collector/src/main/java/kernel360/ckt/collector/CollectorApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-
-@EntityScan(basePackages = "kernel360.ckt.core")
 @SpringBootApplication
 public class CollectorApplication {
     public static void main(String[] args) {
