8fc65ed63e9832b71bb1e75861d3504ed019907a
maximum0 <maximum.zero95@gmail.com>
2025-06-26 14:24:06 +0900

    refactor: collector 모듈 request 및 command 리펙토링
    - request 유효성 검증 추가
    - command 객체 record 로 변환

--- collector/src/main/java/kernel360/ckt/collector/application/service/command/VehicleCollectorCycleCommand.java
+++ collector/src/main/java/kernel360/ckt/collector/application/service/command/VehicleCollectorCycleCommand.java
@@ -1,20 +1,15 @@
 package kernel360.ckt.collector.application.service.command;
 
 import kernel360.ckt.core.domain.dto.CycleInformation;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public class VehicleCollectorCycleCommand {
-    private final Long vehicleId;
-    private final LocalDateTime onTime;
-    private final List<CycleInformation> cList;
-
+public record VehicleCollectorCycleCommand(
+    Long mdn,
+    LocalDateTime onTime,
+    List<CycleInformation> cList
+) {
     public static VehicleCollectorCycleCommand create(
         Long vehicleId,
         LocalDateTime onTime,
--- collector/src/main/java/kernel360/ckt/collector/application/service/command/VehicleCollectorOnCommand.java
+++ collector/src/main/java/kernel360/ckt/collector/application/service/command/VehicleCollectorOnCommand.java
@@ -1,40 +1,31 @@
 package kernel360.ckt.collector.application.service.command;
 
 import java.time.LocalDateTime;
+
+import jakarta.annotation.Nullable;
+import kernel360.ckt.core.domain.dto.GCD;
 import kernel360.ckt.core.domain.entity.DrivingLogEntity;
 import kernel360.ckt.core.domain.entity.RentalEntity;
 import kernel360.ckt.core.domain.entity.RouteEntity;
 import kernel360.ckt.core.domain.entity.VehicleEntity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public class VehicleCollectorOnCommand {
-    private final Long vehicleId;
-    private final String gcd;
-    private final double lat;
-    private final double lon;
-    private final long direction;
-    private final long speed;
-    private final long totalDistance;
-    private final LocalDateTime onTime;
-
+public record VehicleCollectorOnCommand(
+    Long mdn,
+    GCD gcd,
+    double lat,
+    double lon,
+    int ang,
+    int spd,
+    long totalDistance,
+    LocalDateTime onTime
+) {
     public static VehicleCollectorOnCommand create(
-        Long vehicleId,
-        String gcd,
-        double lat,
-        double lon,
-        long direction,
-        long speed,
-        long totalDistance,
-        LocalDateTime onTime
+        Long mdn, GCD gcd, double lat, double lon, int ang, int spd, long totalDistance, LocalDateTime onTime
     ) {
-        return new VehicleCollectorOnCommand(vehicleId, gcd, lat, lon, direction, speed, totalDistance, onTime);
+        return new VehicleCollectorOnCommand(mdn, gcd, lat, lon, ang, spd, totalDistance, onTime);
     }
 
-    public DrivingLogEntity toDrivingLogEntity(RentalEntity rental, VehicleEntity vehicle) {
+    public DrivingLogEntity toDrivingLogEntity(@Nullable RentalEntity rental, VehicleEntity vehicle) {
         return DrivingLogEntity.create(rental, vehicle);
     }
 
