e09a13f883f541dd95a6f3ff95d22f41aeef9c50
maximum0 <maximum.zero95@gmail.com>
2025-07-11 09:59:19 +0900

    fix: @Profile 분리
    - 기존 consumer 에서 publisher 및 consumer 로 분리

--- collector/src/main/java/kernel360/ckt/collector/ui/SseController.java
+++ collector/src/main/java/kernel360/ckt/collector/ui/SseController.java
@@ -1,15 +1,16 @@
 package kernel360.ckt.collector.ui;
 
 import kernel360.ckt.collector.config.SseEmitterManager;
-import kernel360.ckt.collector.consumer.GpsViewConsumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
+@Profile("consumer")
 @RestController
 @RequestMapping("/api/v1/sse")
 @RequiredArgsConstructor
