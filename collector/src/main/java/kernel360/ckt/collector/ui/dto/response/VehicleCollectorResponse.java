dc7221b70882130397e81e8f721bcbf18f4da044
maximum0 <maximum.zero95@gmail.com>
2025-07-11 09:57:43 +0900

    fix: 주기정보 API Response 형식 변경
    - 애뮬레이터 호출시 형식이 맞지 않아 해당 response 를 기존 형태로 변경하였습니다.

--- collector/src/main/java/kernel360/ckt/collector/ui/dto/response/VehicleCollectorResponse.java
+++ collector/src/main/java/kernel360/ckt/collector/ui/dto/response/VehicleCollectorResponse.java
@@ -3,6 +3,10 @@ package kernel360.ckt.collector.ui.dto.response;
 public record VehicleCollectorResponse(
     String mdn
 ) {
+    public static VehicleCollectorResponse from() {
+        return new VehicleCollectorResponse(null);
+    }
+
     public static VehicleCollectorResponse from(Long vehicleId) {
         return new VehicleCollectorResponse(vehicleId.toString());
     }
